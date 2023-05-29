49508a1a3c9fc463107a24667ed1189ae1f9ac7e
Vianey Alejandra <124898114+VianiGuerra03@users.noreply.github.com>
2023-05-29 08:29:49 -0600

    Viani_Semillas

---
+++ Viani_semillas.R
@@ -0,0 +1,11 @@
+# t-test dependientes 
+ semilla <- read.csv("mainproduccion.csv", header = T)
+semilla$Tiempo <- as.factor(semilla$Tiempo) 
+# Grafica boxplot
+boxplot(semilla$Tiempo , semilla$Kgsem)
+boxplot(semilla$BioRama , semilla$Tiempo) 
+boxplot(semilla$Kgsem)
+t.test(semilla$Kgsem ~ semilla$Tiempo, paired = TRUE)
+t.test(semilla$Germ ~ semilla$Tiempo, paired =T)
+t.test(semilla$BioRama ~ semilla$Tiempo, paired=T)
+t.test(semilla$H6 ~ semilla$Tiempo, paired=T)
