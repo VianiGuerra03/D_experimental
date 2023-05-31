e33487f42a2a1c8efb67746f0747169461d40462
Vianey Alejandra <124898114+VianiGuerra03@users.noreply.github.com>
2023-05-31 14:02:44 -0600

    Add files via upload

---
+++ scripts/gastos.R
@@ -0,0 +1,11 @@
+# TAREA DE CALE 
+# VAGM 22/02/23 OBJETO Y VALOR
+transporte <- 550
+comida <- 1000
+alquiler <- 2200
+gimnasio <- 600
+transporte+comida+alquiler+gimnasio
+4350*5
+21750*2 
+gastos <-c(transporte, comida, alquiler, gimnasio)
+barplot(gastos)
