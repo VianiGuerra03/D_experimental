0369ab014adba72039ab003b16f473207990254e
Vianey Alejandra <124898114+VianiGuerra03@users.noreply.github.com>
2023-05-31 13:37:20 -0600

    Add files via upload

---
+++ scripts/viani_profeppa.R
@@ -0,0 +1,14 @@
+# VAGM
+# IMPORTAR
+profepa_url <- "http://www.profepa.gob.mx/innovaportal/file/7635/1/accionesInspeccionfoanp.csv"
+profepaaa <- read.csv(profepa_url, encoding = "latin1")
+prof.ur12 <- paste0((""))
+summary(profepaaa)
+profepaaa$Operativo == max(profepaaa$Operativo)
+max.oper <- subset(profepaaa, profepaaa$Operativo == max(profepaaa$Operativo))
+boxplot(profepaaa$Recorrido)
+profepa.SOC <- subset(profepaaa, profepaaa$Operativo != max(profepaaa$Operativo))
+boxplot(profepa.SOC$Operativo)
+library(repmis)
+conjunto <- source_data("https://www.dropbox.com/s/hmsf07bbayxv6m3/cuadro1.csv?d1=1")
+conjunto$Especie <- as.factor(conjunto$Especie)
